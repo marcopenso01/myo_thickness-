3c6636261a25a8ccdf2f9e2a01480b5fab4a45fe
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-09-27 14:43:56 +0200

    Update model_zoo.py

--- model_zoo.py
+++ model_zoo.py
@@ -7,49 +7,51 @@ from tfwrapper import layers as lr
 #same
 def unet2D_same(input_tensor, nlabels):
     
-    inputs = layers.InputLayer(input_shape=(input_tensor.shape[1], input_tensor.shape[2], 1))
-    conv1_1 = lr.conv2D_layer_bn(inputs, num_filters=64)
-    conv1_2 = lr.conv2D_layer_bn(conv1_1, num_filters=64)
+    inputs = tf.keras.Input(shape=(input_tensor.shape[1], input_tensor.shape[2], 1))
+    print('inputs', inputs.shape)
+    conv1_1 = lr.conv2D_layer_bn(inputs, num_filters=48)
+    print('conv1_1', conv1_1.shape)
+    conv1_2 = lr.conv2D_layer_bn(conv1_1, num_filters=48)
     pool1 = lr.max_pool_layer2d(conv1_2)
 
-    conv2_1 = lr.conv2D_layer_bn(pool1, num_filters=128)
-    conv2_2 = lr.conv2D_layer_bn(conv2_1, num_filters=128)
+    conv2_1 = lr.conv2D_layer_bn(pool1, num_filters=96)
+    conv2_2 = lr.conv2D_layer_bn(conv2_1, num_filters=96)
     pool2 = lr.max_pool_layer2d(conv2_2)
 
-    conv3_1 = lr.conv2D_layer_bn(pool2, num_filters=256)
-    conv3_2 = lr.conv2D_layer_bn(conv3_1, num_filters=256)
+    conv3_1 = lr.conv2D_layer_bn(pool2, num_filters=192)
+    conv3_2 = lr.conv2D_layer_bn(conv3_1, num_filters=192)
     pool3 = lr.max_pool_layer2d(conv3_2)
 
-    conv4_1 = lr.conv2D_layer_bn(pool3, num_filters=512)
-    conv4_2 = lr.conv2D_layer_bn(conv4_1, num_filters=512)
+    conv4_1 = lr.conv2D_layer_bn(pool3, num_filters=384)
+    conv4_2 = lr.conv2D_layer_bn(conv4_1, num_filters=384)
     pool4 = lr.max_pool_layer2d(conv4_2)
 
-    conv5_1 = lr.conv2D_layer_bn(pool4, num_filters=1024)
-    conv5_2 = lr.conv2D_layer_bn(conv5_1, num_filters=1024)
+    conv5_1 = lr.conv2D_layer_bn(pool4, num_filters=768)
+    conv5_2 = lr.conv2D_layer_bn(conv5_1, num_filters=768)
     upconv4 = lr.upsample(conv5_2)
 
     concat4 = layers.concatenate([conv4_2, upconv4], axis=-1)
 
-    conv6_1 = lr.conv2D_layer_bn(concat4, num_filters=512)
-    conv6_2 = lr.conv2D_layer_bn(conv6_1, num_filters=512)
+    conv6_1 = lr.conv2D_layer_bn(concat4, num_filters=384)
+    conv6_2 = lr.conv2D_layer_bn(conv6_1, num_filters=384)
     upconv3 = lr.upsample(conv6_2)
     
     concat3 = layers.concatenate([conv3_2, upconv3], axis=-1)
 
-    conv7_1 = lr.conv2D_layer_bn(concat3, num_filters=256)
-    conv7_2 = lr.conv2D_layer_bn(conv7_1, num_filters=256)
+    conv7_1 = lr.conv2D_layer_bn(concat3, num_filters=192)
+    conv7_2 = lr.conv2D_layer_bn(conv7_1, num_filters=192)
     upconv2 = lr.upsample(conv7_2)
     
     concat2 = layers.concatenate([conv2_2, upconv2], axis=-1)
 
-    conv8_1 = lr.conv2D_layer_bn(concat2, num_filters=128)
-    conv8_2 = lr.conv2D_layer_bn(conv8_1, num_filters=128)
+    conv8_1 = lr.conv2D_layer_bn(concat2, num_filters=96)
+    conv8_2 = lr.conv2D_layer_bn(conv8_1, num_filters=96)
     upconv1 = lr.upsample(conv8_2)
     
     concat1 = layers.concatenate([conv1_2, upconv1], axis=-1)
 
-    conv9_1 = lr.conv2D_layer_bn(concat1, num_filters=64)
-    conv9_2 = lr.conv2D_layer_bn(conv9_1, num_filters=64)
+    conv9_1 = lr.conv2D_layer_bn(concat1, num_filters=48)
+    conv9_2 = lr.conv2D_layer_bn(conv9_1, num_filters=48)
     
     if nlabels == 2:
         pred = lr.conv2D_layer_bn(conv9_2, num_filters=1, kernel_size=(1,1), activation='sigmoid')
